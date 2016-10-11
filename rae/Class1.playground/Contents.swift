d794b54b8fe7f3d2a674d23c1ff51e2b25df5bd4
Rae Helsel <rahelsel@me.com>
2016-10-11 09:06:36 -0700

    homework

--- rae/Class1.playground/Contents.swift
+++ rae/Class1.playground/Contents.swift
@@ -3,3 +3,33 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+func checkAndReturn(myString: String) -> Int? {
+    print(myString)
+    
+    return Int(myString)
+    
+}
+
+let result = checkAndReturn(myString: "abc")
+
+
+
+func addTwoInt(number: Int) -> Int {
+    let addition = number + number
+    return addition
+}
+addTwoInt(number: 2)
+
+let addTuple: (Int, Int) -> () = {(numberOne, numberTwo) in
+    print(numberOne + numberTwo)
+}
+
+addTuple(2, 4)
+
+
+let myString : String = "\(number)"
+func printStringMsg(myString: String) -> String {
+    print("This string has \(myString.characters.count) characters")
+}
